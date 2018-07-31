10d5d5759a4f0e07cb49c3d1528599f09db36893
James Buncle <jbuncle@hotmail.com>
2018-07-31 21:05:01 +0100

    Splitter working on window resize

--- src/app/components/vertical-splitter/vertical-splitter.component.ts
+++ src/app/components/vertical-splitter/vertical-splitter.component.ts
@@ -1,22 +1,38 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild, ContentChildren, ElementRef, AfterViewInit, HostListener} from '@angular/core';
 
 @Component({
     selector: 'app-vertical-splitter',
     templateUrl: './vertical-splitter.component.html',
     styleUrls: ['./vertical-splitter.component.css']
 })
-export class VerticalSplitterComponent implements OnInit {
+export class VerticalSplitterComponent implements AfterViewInit {
 
-    constructor() {}
 
-    ngOnInit() {
+    private splitter: Element;
+
+    private lastPosition = 200;
+    constructor(
+        private rootEl: ElementRef
+    ) {
+
+
+    }
+    ngAfterViewInit(): void {
+        const rootElement: HTMLElement = this.rootEl.nativeElement;
+        this.splitter = rootElement.getElementsByClassName("splitter")[0];
+
     }
 
     left: number;
-    right: number;
 
+    minLeft: number = 100;
+    minRight: number = 100;
+
+    @HostListener('window:resize', ['$event'])
     resize(event) {
         console.log(event);
+        console.log(this.lastPosition)
+        this.calcPosition(this.lastPosition);
     }
 
     splitterDrag(event: DragEvent) {
@@ -24,12 +40,38 @@ export class VerticalSplitterComponent implements OnInit {
 
         const position: number = +(event.screenX);
 
-        if (position > 0) {
+        this.calcPosition(position);
+    }
 
-            this.left = position - event.srcElement.parentElement.getBoundingClientRect().left;
-            this.right = event.srcElement.parentElement.getBoundingClientRect().width
-                - this.left
-                - event.srcElement.getBoundingClientRect().width;
+    private calcPosition(position: number) {
+        if (position > 0) {
+            this.lastPosition = position;
+            const parentWidth: number = this.splitter.parentElement.getBoundingClientRect().width;
+            const splitterWidth: number = this.splitter.getBoundingClientRect().width;
+
+            let newLeft: number = position - this.splitter.parentElement.getBoundingClientRect().left;
+
+            if (newLeft >= parentWidth) {
+                // Splitter has gone out of box
+                newLeft = parentWidth - splitterWidth - this.minRight;
+            }
+
+            if (newLeft < this.minLeft) {
+                newLeft = this.minLeft;
+            }
+
+            let newRight = parentWidth - newLeft - splitterWidth;
+            if (newRight < this.minRight) {
+                newLeft = parentWidth - this.minRight - splitterWidth;
+                newRight = parentWidth - newLeft - splitterWidth;
+            }
+
+            if (newLeft < this.minLeft || newRight < this.minRight) {
+                newLeft = (parentWidth - splitterWidth) / 2;
+                newRight = (parentWidth - splitterWidth) / 2;
+            }
+
+            this.left = newLeft;
         }
     }
 
@@ -38,4 +80,5 @@ export class VerticalSplitterComponent implements OnInit {
         img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=';
         event.dataTransfer.setDragImage(img, 0, 0);
     }
+
 }
